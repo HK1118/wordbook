d9cad77e219d523bffc88dd934802317eaef145a
KoutaroY <5106koutaro@gmail.com>
2024-09-14 16:21:01 +0000

    9/14 finish

--- start.js
+++ start.js
@@ -3,3 +3,13 @@ function buttonClick(){
     alert(number)
     window.location.href = "second.html";
 }
+
+function buttonClick2(){
+    var text = document.getElementById('text').value;
+    alert(text)
+    var next_text = text.split('//');
+    alert(next_text)
+    var q_or_a = next_text.split('/');
+    alert(q_or_a)
+}
+
