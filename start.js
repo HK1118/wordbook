b7f5f707b00e74e87c2c9b2726ef56a2451d641f
K_Y.5106 <5106koutaro@gmail.com>
2024-09-16 23:37:27 +0900

    split can't understand

--- start.js
+++ start.js
@@ -1,8 +1,28 @@
 function buttonClick2(){
     var text = document.getElementById('text').value;
     alert(text)
-    var text2 = test.split(/\n/);
+    console.log("出力テスト１");
+    var text2 = text.split(/\n/);       //改行(問題ごと)で分割
     alert(text2)
+    console.log("出力テスト2");
+    alert(text2.length)
     console.log(text2.length);
-    alert("text2.length")
+    var q_number = text2.length;        //問題の個数
+    alert("個数"+q_number)
+
+    var q_a = text2.split(/\s/);
+    alert(q_a) 
+
+    let i = 0;
+    do{
+        i += 1;
+        alert(i)
+        alert(text2)
+        console.log(i);
+        var q_a = text2.split(/\s/);    //空白で分割
+        alert(q_a)
+    } while(i<q_number)
+    
+    alert(q)
+    alert(a)
 }
