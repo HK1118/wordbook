ae357cbd2a701e1c42ca77d66eb2bf7aa1d854fd
KoutaroY <5106koutaro@gmail.com>
2024-09-17 03:49:20 +0000

    あああああ

--- start.js
+++ start.js
@@ -10,19 +10,21 @@ function buttonClick2(){
     var q_number = text2.length;        //問題の個数
     alert("個数"+q_number)
 
-    var q_a = text2.split(/\s/);
-    alert(q_a) 
-
+    
     let i = 0;
     do{
         i += 1;
         alert(i)
         alert(text2)
         console.log(i);
-        var q_a = text2.split(/\s/);    //空白で分割
+        var q_a = text2.split("\s");    //空白で分割
         alert(q_a)
     } while(i<q_number)
     
     alert(q)
     alert(a)
+
+    var q_a = text2.split(/\s/);
+    alert(q_a) 
+
 }
