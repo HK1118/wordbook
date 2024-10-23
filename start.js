2684dd5f1938ecdd273fe2d0859d0bcdcf0ec2c2
KoutaroY <5106koutaro@gmail.com>
2024-10-23 01:42:40 +0000

    hぐcghdh

--- start.js
+++ start.js
@@ -18,11 +18,14 @@ function buttonClick10() {
     navigator.clipboard.writeText('q1 a1\nq2 a2\nq3 a3\nq4 a4\nq5 a5\nq6 a6\nq7 a7\nq8 a8\nq9 a9\nq10 a10');
     //alert("コピーしました！(10)")
 }
+function Japanese_English(){
+    navigator.clipboard.writeText('英語 English 日本語 Japanese 犬 dog 猫 cat')
+}
 
 function copy_word() {
     navigator.clipboard.writeText('1+1= 2 2+2= 4 3+3= 6 4+4= 8 5+5= 10 6+6= 12 7+7= 14 8+8= 16 9+9= 18 10+10= 20');
     //alert("コピーしました！(足し算")
-    party.confetti(runButton,{
+    party.confetti(runButton, {
         count: party.variation.range(20, 40),
     });
 }
@@ -44,10 +47,10 @@ function buttonClick2() {
         let j = Math.floor(Math.random() * (i + 1));
         [question_numbers[i], question_numbers[j]] = [question_numbers[j], question_numbers[i]];
     }
-    
+
     localStorage.setItem('question_numbers', question_numbers);
-    localStorage.setItem('correct_count',null);
-    localStorage.setItem('miss_count',null);
+    localStorage.setItem('correct_count', null);
+    localStorage.setItem('miss_count', null);
 
 }
 
@@ -104,7 +107,7 @@ function countQPageVisits() {
 // Q.htmlに遷移する関数
 function AA() {
     countQPageVisits(); // ページ遷移の回数をカウント
-    
+
     check();
 }
 
@@ -128,49 +131,40 @@ function count_reset() {
 function page_A() {
     let random = localStorage.getItem('question_numbers');
     let count = parseInt(localStorage.getItem('QPageVisitCount')) || 0;
-    function AAA() {
-        var aaaa = document.getElementById('A_number');
-        var count2 = count + 1;
-        aaaa.textContent = `第${count2}問`;
-    }
-    AAA();
-    function A() {
-        var random2 = random.split(',');
-        var random3 = random2[count];
-        var A1 = document.getElementById('A');
-        var AA = localStorage.getItem('input_texts');
-        var AAA = AA.split(",");
-        var random4 = random3 * 2 + 1;
-        A1.textContent = `正しい答え:　　${AAA[random4]}`;
-        localStorage.setItem('correct',AAA[random4]);
-    }
-    A();
-    function YourA() {
-        let yourA = localStorage.getItem('yourA');
-        var A = document.getElementById('yourA');
-        A.textContent = `あなたの回答:　${yourA}`;
-        localStorage.setItem('yourA',yourA);
-    }
-    YourA();
 
-    function correct_or_miss(){
-        var correct = localStorage.getItem('correct');
-        var yourA = localStorage.getItem('yourA');
-        var correct_miss = document.getElementById('correct_miss');
-        var correct_count = parseInt(localStorage.getItem('correct_count')) || 0;
-        var miss_count = parseInt(localStorage.getItem('miss_count')) || 0;
-        
-        if (yourA == correct){
-            correct_miss.textContent = "正解！！！";
-            correct_count += 1;
-            localStorage.setItem('correct_count',correct_count);
-        }else{
-            correct_miss.textContent = "不正解...";
-            miss_count += 1;
-            localStorage.setItem('miss_count',miss_count);
-        }
+    var aaaa = document.getElementById('A_number');
+    var count2 = count + 1;
+    aaaa.textContent = `第${count2}問`;
+
+    var random2 = random.split(',');
+    var random3 = random2[count];
+    var A1 = document.getElementById('A');
+    var AA = localStorage.getItem('input_texts');
+    var AAA = AA.split(",");
+    var random4 = random3 * 2 + 1;
+    A1.textContent = `正しい答え:　　${AAA[random4]}`;
+    localStorage.setItem('correct', AAA[random4]);
+
+    let yourA = localStorage.getItem('yourA');
+    var A = document.getElementById('yourA');
+    A.textContent = `あなたの回答:　${yourA}`;
+    localStorage.setItem('yourA', yourA);
+
+    var correct = localStorage.getItem('correct');
+    var correct_miss = document.getElementById('correct_miss');
+    var correct_count = parseInt(localStorage.getItem('correct_count')) || 0;
+    var miss_count = parseInt(localStorage.getItem('miss_count')) || 0;
+
+    if (yourA == correct) {
+        correct_miss.textContent = "正解！！！";
+        correct_count += 1;
+        localStorage.setItem('correct_count', correct_count);
+    } else {
+        correct_miss.textContent = "不正解...";
+        miss_count += 1;
+        localStorage.setItem('miss_count', miss_count);
     }
-    correct_or_miss();
+
 }
 
 //ぺーーーーじ遷移ーーーー！！最終！！！！！！
@@ -194,28 +188,37 @@ function fin() {
     miss.textContent = `不正解数:   ${miss_count}`;
 }
 
-function pressKey(key){
+function pressKey(key) {
     const your_A = document.getElementById('your_A');
     your_A.value += key;
 }
-function pressKey_s(key){
+function pressKey_s(key) {
     const text = document.getElementById('input_text');
     text.value += key;
 }
-function pressKey_s_delete(){
+function pressKey_s_delete() {
     let text = document.getElementById('input_text');
-    text.value = text.value.slice(0,-1);
+    text.value = text.value.slice(0, -1);
 }
-function pressKey_delete(){
+function pressKey_delete() {
     let your_A = document.getElementById('your_A');
-    your_A.value = your_A.value.slice(0,-1);
+    your_A.value = your_A.value.slice(0, -1);
 }
-function pressKey_s_clear(){
-    let text = document.getElementById('text');
+function pressKey_s_clear() {
+    let text = document.getElementById('input_text');
     text.value = "";
 }
-function pressKey_clear(){
+function pressKey_clear() {
     let your_A = document.getElementById('your_A');
     your_A.value = "";
 }
 
+function pressKey_s_shift(){
+    document.getElementById('keypad_s_alpha').classList.add('hidden');
+    document.getElementById('keypad_s_ALPHA').classList.remove('hidden');
+
+}
+function pressKey_s_SHIFT(){
+    document.getElementById('keypad_s_ALPHA').classList.add('hidden');
+    document.getElementById('keypad_s_alpha').classList.remove('hidden');
+}
